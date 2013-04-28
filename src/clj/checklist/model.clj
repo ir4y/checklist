aa02cb51beba6e3173f2c616a6eeac831915362b
Ilya Beda <ir4y.ix@gmail.com>
2013-04-29 00:23:49 +0800

    Syntax fix

--- src/clj/checklist/model.clj
+++ src/clj/checklist/model.clj
@@ -2,13 +2,13 @@
     (:require [taoensso.carmine :as car]
               [checklist.util :as util]))
 
+
 (def pool         (car/make-conn-pool)) ; See docstring for additional options
 (def spec-server1 (car/make-conn-spec))
 
+
 (defmacro wcar [& body] `(car/with-conn pool spec-server1 ~@body))
 
-(defn get_links []
-  (wcar (car/lrange "links" 0 -1)))
 
 (defn get-uuid []
   (loop[uuid (util/new-uuid)]
@@ -16,28 +16,33 @@
       uuid
       (recur util/new-uuid))))
 
+
 (defn insert-check [check_text]
   (let [uuid (get-uuid)]
     (wcar (car/hset uuid "text" check_text))
     (wcar (car/hset uuid "done" false))
     uuid))
 
+
 (defn get-check [uuid]
   (wcar
     [(car/hget uuid "text") (car/hget uuid "done")]))
 
+
 (defn set-done [uuid done]
   (wcar (car/hset uuid "done" done)))
+
 
 (defn to-hash [uuid]
   (let [result (get-check uuid)]
     {:lookup "fake" :uuid uuid :values {:text (get result 0) :done (get result 1)}}))
+
 
 (defn get-checklists []
   (map to-hash (wcar (car/smembers "scheck"))))
 
 
 (defn delete-check [uuid]
-  (wcar (car (srem "scheck" uuid))
-        (car (hdel uuid "text"))
-        (car (hdel uuid "done"))))
+  (wcar (car/srem "scheck" uuid)
+        (car/hdel uuid "text")
+        (car/hdel uuid "done")))
