8da109f2e170dce0c89f51408eebcff01098484d
Ilya Beda <ir4y.ix@gmail.com>
2013-12-11 21:47:34 +0800

    Update deps, use http-kit

--- project.clj
+++ project.clj
@@ -2,33 +2,23 @@
   :description "Simple ajax checklist application"
   :url "http://checklist.allsol.ru/"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [compojure "1.1.5"]
-                 [com.taoensso/carmine "1.6.0"]
+                 [compojure "1.1.6"]
+                 [com.taoensso/carmine "2.4.0"]
                  [enfocus "1.0.1"]
                  [jayq "2.3.0"]
                  [ring/ring-json "0.2.0"]
-                 ]
-  :plugins [[lein-ring "0.8.3"]
-            [lein-cljsbuild "0.3.0"]
-            [lein-tarsier "0.10.0"]]
-  :ring {:handler checklist.handler/app}
-  :profiles
-  {:dev {:dependencies [[ring-mock "0.1.3"]]}}
-  
-  
-  :aot []
+                 [http-kit "2.1.13"]]
+  :plugins [[lein-cljsbuild "0.3.0"]]
   :source-paths ["src/clj" "src/cljs"]
-  
-  :cljsbuild
-  {:builds
-   [
-    {:source-paths ["src/cljs"],
-     :id "main",
-     :compiler
-     {:pretty-print true,
-      :output-to "resources/public/js/main.js",
-      :warnings true,
-      :externs ["externs/jquery-1.9.js"],
-      ;; :optimizations :advanced,
-      :optimizations :whitespace,
-      :print-input-delimiter false}}]})
+  :main checklist.handler
+  :profiles {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]]}}
+  :cljsbuild {:builds
+              [
+               {:source-paths ["src/cljs"],
+                :id "main",
+                :compiler {:pretty-print true,
+                           :output-to "resources/public/js/main.js",
+                           :warnings true,
+                           :externs ["externs/jquery-1.9.js"],
+                           :optimizations :whitespace,
+                           :print-input-delimiter false}}]})
--- src/clj/checklist/handler.clj
+++ src/clj/checklist/handler.clj
@@ -2,6 +2,7 @@
   (:use compojure.core)
   (:require [compojure.handler :as handler]
             [compojure.route :as route]
+            [org.httpkit.server :as kit]
             [checklist.model :as model]))
 
 (use 
@@ -9,6 +10,7 @@
   'ring.util.response)
 
 (defroutes app-routes
+  (GET "/" [] (resource-response "index.html" {:root "public"}))
   (GET  "/checklist" [] (response (model/get-checklists)))
   (POST "/set_done" [uuid done] (response {:result (model/set-done uuid done)}))
   (POST "/new_check" [text] (response (model/insert-check text)))
@@ -16,5 +18,7 @@
   (route/resources "/")
   (route/not-found "Not Found"))
 
-(def app
-  (wrap-json-response (handler/site app-routes)))
+
+(defn -main [& args]
+  (kit/run-server (wrap-json-response
+                    (handler/site #'app-routes)) {:port 8080}))
